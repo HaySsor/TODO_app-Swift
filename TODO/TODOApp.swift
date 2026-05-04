54952a2773c63f1d503b0204e24b56f8d48d547a
hayss007@gmail.com <hayss007@gmail.com>
2026-05-04 18:37:57 +0200

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
