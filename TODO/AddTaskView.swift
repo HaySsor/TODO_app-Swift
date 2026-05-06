1d0fce3b31a53896dc4127fa08a509c5e75dc70b
hayss007@gmail.com <hayss007@gmail.com>
2026-05-06 23:10:06 +0200

    test xocode integrate

--- TODO/AddTaskView.swift
+++ TODO/AddTaskView.swift
@@ -34,7 +34,7 @@ struct AddTaskView: View {
                                 taskColor = color
                             }
                     }
-                }
+                } 
                 .listRowBackground(Color.clear)
                 .listRowSeparator(.hidden)
 
