1c75740f74d102649a428fc769de5124d997fec0
hayss007@gmail.com <hayss007@gmail.com>
2026-05-17 13:58:59 +0200

    move data to SwiftData add some animations, move init logic to TODO app ad edit view and move form to new componet used in Add and edit task

---
+++ TODO/Views/EditTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskView.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 17/05/2026.
+//
+
+import SwiftUI
+
+struct EditTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTaskView()
+}
