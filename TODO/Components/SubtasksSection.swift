5ef2b4abf94590805056bc3cdbf22793475642e2
hayss007@gmail.com <hayss007@gmail.com>
2026-05-29 23:17:57 +0200

    add function to pin task and display pinned task on top TodayView and All View

---
+++ TODO/Components/SubtasksSection.swift
@@ -0,0 +1,18 @@
+//
+//  SubtasksSection.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 29/05/2026.
+//
+
+import SwiftUI
+
+struct SubtasksSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubtasksSection()
+}
