2fabc29fb7e687983864ec3f40634428d40ec6ab
hayss007@gmail.com <hayss007@gmail.com>
2026-06-02 20:52:33 +0200

    start adding Stats View

---
+++ TODO/Components/HeatMap.swift
@@ -0,0 +1,18 @@
+//
+//  HeatMap.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 02/06/2026.
+//
+
+import SwiftUI
+
+struct HeatMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeatMap()
+}
---
+++ TODO/Views/StatsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatsView.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 02/06/2026.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
