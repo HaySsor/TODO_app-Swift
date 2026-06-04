a9845a243fe616664df0510cd587fa50e7b0bf71
hayss007@gmail.com <hayss007@gmail.com>
2026-06-04 21:13:52 +0200

    add stats page

---
+++ StatBox.swift
@@ -0,0 +1,18 @@
+//
+//  StatBox.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 04/06/2026.
+//
+
+import SwiftUI
+
+struct StatBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatBox()
+}
---
+++ TODO/Components/CategoryChart.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryChart.swift
+//  TODO
+//
+//  Created by Przemek Hussar on 04/06/2026.
+//
+
+import SwiftUI
+
+struct CategoryChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryChart()
+}
